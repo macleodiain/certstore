67c194edfab187cb9b0305d67aa3453848dc3af8
Iain Macleod <macleod@live.co.uk>
2025-01-21 02:43:36 +0000

    update migration default

--- db/migrate/20250119212528_add_user_id_to_certs.rb
+++ db/migrate/20250119212528_add_user_id_to_certs.rb
@@ -1,5 +1,5 @@
 class AddUserIdToCerts < ActiveRecord::Migration[8.0]
   def change
-    add_reference :certs, :user, null: false, foreign_key: true
+    add_reference :certs, :user, null: false, foreign_key: true, default: 1
   end
 end
